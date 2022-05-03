63ac84c7bde078ddd2b19bf3fc9df071cf846cc6
Aluno IFAL <alunoifal@IFAL.LOCAL>
2022-05-03 18:11:01 -0300

    operadores decimais e expressoes organizadas

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,6 +1,6 @@
 function atualizarDisplay(btn){
     const display = document.getElementById('display');
-    if(display.value.length === 9) return;
+    if(display.value.length === 15  ) return;
     if(display.value === '0') display.value = btn.value;
     else display.value += btn.value;
 }
@@ -14,19 +14,32 @@ var valor1 = 0
 function atualizarOperacao(btn){
     const display = document.getElementById('display');
     operador = btn.value;
-    valor1 = parseInt(display.value);
-    display.value = '0';
+    valor1 = parseFloat(display.value);
+    display.value += operador;
 }
 
 function calcularOperacao(){
     const display = document.getElementById('display');
-    const valor2 = parseInt(display.value);
-    valor1 = eval(valor1+operador+valor2);
-    display.value = valor1;
-    operador = '';
+    const valor2 = parseFloat(display.value);
+        if(operador != "%"){
+            valor1 = eval(display.value);
+            display.value = valor1;
+            operador = '';
+        }else{
+            valor1 = Math.sqrt(display.value);
+            display.value = valor1;
+            operador = '';
+        }
 }       
 
+function OperacaoRaiz(btn){
+    const display = document.getElementById('display');
+    operador = btn.value;
+    display.value = ""; 
+    display.value = operador;
+}
+
 function manipularTeclado(){
-    if(/[0-9]/.test(event.key))
-        atualizarDisplay({value: event.key});
+    if(/[0-9 / * - + . ( )]/.test(event.key))
+    atualizarDisplay({value: event.key});
 }
